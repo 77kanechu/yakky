79599f4fb7a74dbf53896776f6869705da6dd279
kanechutenkabu <kanechutenkabu@gmail.com>
2024-12-12 11:48:00 +0900

    ~

--- assets/program.js
+++ assets/program.js
@@ -17,12 +17,12 @@ async function loadData() {
         days[times] = x;
         yaksiobas[times] = y;
         yoreis[times] = z;
-        yakkyvalue[times] = (16 - y + z);
+        yakkyvalue[times] = (16 + 4 * y - 4 * z);
         times--;
     });
     document.getElementById("yvalue").innerText = yakkyvalue[6];
-    document.getElementById("gvalue").innerText = (yakkyvalue[6] / 16);
-    document.getElementById("lastvalue").innerText = yakkyvalue[5];
+    document.getElementById("gvalue").innerText = (yakkyvalue[6] / 1600 * 100);
+    document.getElementById("lastvalue").innerText = yakkyvalue[6] / yakkyvalue[5] / 100 * 10000 + "%";
     document.getElementById("ate").innerText = yaksiobas[6];
     document.getElementById("late").innerText = yoreis[6];
     var yakky = new Chart(yakkyctx, {
